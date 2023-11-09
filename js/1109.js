b28acbfc9faf6adbe56418ad5a61eedda5923c0e
yuna-c <yuna744@gmail.com>
2023-11-09 10:47:54 +0900

    1109 함수에 리턴값이 필요한 이유

--- js/1109.js
+++ js/1109.js
@@ -12,6 +12,18 @@
   : 매개변수 - 외부의 같은 함수내부로 전달하기 위한 통로명
   : 인수 - 함수의 매개변수를 통해서 전달되는 값 자체 
 
+  반환값 (return)
+  : 함수 내부에서 만들어진 값을 함수 외부로 물리적으로 내보냄
+  : 함수 코드블록 안에서 만들어진 값은 외부에서 접근불가(지역스코프,지역변수)
+  
+  반환값이 필요한 이유 
+  : 함수 내부에서 만들어진 값이 일회성으로 소비되는 것이 아닌
+  : 추후 다른 형태로 재사용이 되야될 때 함수 외부로 값을 내보냄
+  
+  return의 다른 쓰임
+  : 함수 내부에서 return문을 만나면 그 즉시 함수 실행 종료
+  : 함수 내부 로직에서 error가 발생될만한 상황이면 불필요하게 다음 코드들을 실행하지 않고 해당 함수를 강제 종료할 때
+
 */
 
 // 함수의 정의 (name이라는 [매개변수]가 연결되어 있는 함수)
@@ -46,4 +58,31 @@ function plus(num1, num2) {
 	}
 }
 
+// console.log(plus(2, 3)); undefined
 plus(2, 3);
+
+// return 내가해봄
+function plus2(num1, num2) {
+	//코드블록 : 지역변수
+	const result = num1 + num2;
+	return result;
+}
+// console.log(result); error (재활용 안댐)
+// if 2,3 더한값과 4,5를 더한 값을 다시 더해주세요 하면 일회성이니까 호출되고 나서 없어짐
+const returned = plus2(4, 6) + plus2(4, 3);
+console.log(returned);
+
+// return 수업
+function returnPlus(num1, num2) {
+	const result = num1 + num2;
+	return result;
+}
+
+console.log(returnPlus(2, 3));
+// if 2,3 더한값과 4,5를 더한 값을 다시 더해주세요
+
+const result1 = returnPlus(2, 3);
+const result2 = returnPlus(4, 5);
+
+console.log(returnPlus(2, 3) + returnPlus(4, 5));
+console.log(result1 + result2);
